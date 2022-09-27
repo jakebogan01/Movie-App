f7ab59cc60855b6e568a7e3468feb3d52421e22f
Jake Bogan <jakebogan01@gmail.com>
2022-09-26 23:08:42 -0400

    got api working

--- src/js/movie-db.js
+++ src/js/movie-db.js
@@ -1,6 +1,4 @@
 /*************
- SAMPLE URLS
-
  1. To get the config data like image base urls
  https://api.themoviedb.org/3/configuration?api_key=<APIKEY>
 
@@ -11,7 +9,7 @@
  https://api.themoviedb.org/3/movie/<movie-id>?api_key=<APIKEY>
  *************/
 
-const APIKEY = '0497a560599e4b1196149db7ecbc29bb';
+const APIKEY = '0497a560599e4b1196149db7ecbc29bb'; //TMDB api key
 let baseURL = 'https://api.themoviedb.org/3/';
 let configData = null;
 let baseImageURL = null;
@@ -19,50 +17,31 @@ let baseImageURL = null;
 let getConfig = () => {
     let url = "".concat(baseURL, 'configuration?api_key=', APIKEY);
     fetch(url)
-        .then((result) => {
-            return result.json();
-        })
+        .then((result) => {return result.json();})
         .then((data) => {
             baseImageURL = data.images.secure_base_url;
             configData = data.images;
-            console.log('config:', data);
-            console.log('config fetched');
-            runSearch('jaws')
+            getMovies(data);
         })
         .catch(function (err) {
             alert(err);
         });
 }
 
-let runSearch = (keyword) => {
-    // https://api.themoviedb.org/3/movie/popular?api_key=0497a560599e4b1196149db7ecbc29bb&language=en-US
+let getMovies = (images) => {
     let url = ''.concat(baseURL, 'movie/popular?api_key=', APIKEY, '&language=en-US');
     fetch(url)
         .then(result => result.json())
         .then((data) => {
-            //process the returned data
-            // JSON.stringify(data, null, 4)
-            document.getElementById('output').innerHTML = data.results[0].title;
-            //work with results array...
+            console.log('images:', images);
+            console.log('data:', data);
+            let titles = "";
+            for(let i = 0; i < data.results.length; i++){
+                let li = document.createElement("LI");
+                li.innerHTML = `<li>${data.results[i].title}</li>`;
+                document.querySelector(".output").appendChild(li);
+            }
         })
 }
 
-document.addEventListener('DOMContentLoaded', getConfig);
-/*******************************
- SAMPLE SEARCH RESULTS DATA
- { "vote_count": 2762,
-            "id": 578,
-            "video": false,
-            "vote_average": 7.5,
-            "title": "Jaws",
-            "popularity": 16.273358,
-            "poster_path": "/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg",
-            "original_language": "en",
-            "original_title": "Jaws",
-            "genre_ids": [ 27, 53, 12 ],
-            "backdrop_path": "/slkPgAt1IQgxZXNrazEcOzhAK8f.jpg",
-            "adult": false,
-            "overview": "An insatiable great white shark terrorizes the townspeople of Amity Island, The police chief, an oceanographer and a grizzled shark hunter seek to destroy the bloodthirsty beast.",
-            "release_date": "1975-06-18"
-}
- *******************************/
+// document.addEventListener('DOMContentLoaded', getConfig);
