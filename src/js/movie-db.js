487228c2c59666894d2a670f6c08f13fb11b93c8
Jake Bogan <jakebogan01@gmail.com>
2022-09-29 22:49:06 -0400

    small updates

--- src/js/movie-db.js
+++ src/js/movie-db.js
@@ -93,7 +93,7 @@ let getFeaturedMovie = (id) => {
                     <div class="flex space-x-3 mt-2 xl:mt-3 transition-all duration-300">
                         <span class="flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#DEDE50" class="w-4 h-4"><path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" /></svg>
-                            ${Math.floor(data.vote_average)}0%
+                            ${data.vote_average.toFixed(1)}%
                         </span>
                         <span>${data.runtime}min</span>
                         <span>(${newDate})</span>
@@ -146,7 +146,7 @@ let showDetails = (id) => {
                                                                 <svg class="text-yellow-400 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" /></svg>
                                                             </div>
                                                             <p class="text-sm text-white">
-                                                                3.9
+                                                                ${data.vote_average.toFixed(1)}
                                                                 <span class="sr-only"> out of 10 stars</span>
                                                             </p>
                                                         </div>
